811eaa3b5a4fc9db3934d608248592ff12f37564
ltlwill <609738992@qq.com>
2019-05-22 11:16:32 +0800

    by ltl

---
+++ src/test/java/com/efe/kafkaservice/PulsarDemo.java
@@ -0,0 +1,53 @@
+package com.efe.kafkaservice;
+
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.functions.api.Context;
+
+/**
+ * <p>apache pulsar demo: </p> 
+ * @author Liu TianLong
+ * 2019年5月22日 上午11:10:26
+ */
+public class PulsarDemo {
+	
+	public void test1() throws Exception{
+		PulsarClient client = PulsarClient.builder().serviceUrl("pulsar://localhost:6650").build();
+		client.newProducer().createAsync();
+	}
+	
+	public void test2() throws Exception{
+		
+	}
+	
+	/**
+	 * java native 方式
+	 * <p>Abstract: </p> 
+	 * @author Liu TianLong
+	 * 2019年5月22日 上午11:08:37
+	 */
+	public static class NativeCalcWordCount implements java.util.function.Function<String, String>{
+
+		@Override
+		public String apply(String input) { 
+			return String.format("java native function %s!", input);
+		}
+		
+	}
+	
+	/**
+	 * pulsar functions api
+	 * <p>Abstract: </p> 
+	 * @author Liu TianLong
+	 * 2019年5月22日 上午11:10:01
+	 */
+	public static class PulsarCalcWordCount implements org.apache.pulsar.functions.api.Function<String, String>{
+
+		@Override
+		public String process(String input, Context context) throws Exception {
+//			context.getTenant();
+			return String.format("pulsar function %s!", input);
+		}
+		
+	}
+
+}
