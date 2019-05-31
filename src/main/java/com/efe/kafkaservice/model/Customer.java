676f68ff9d7d845b3ed5767e611bc212da17757c
ltlwill <609738992@qq.com>
2019-05-31 14:49:52 +0800

    by ltl

--- src/main/java/com/efe/kafkaservice/model/Customer.java
+++ src/main/java/com/efe/kafkaservice/model/Customer.java
@@ -2,11 +2,13 @@ package com.efe.kafkaservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @SuppressWarnings("serial")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Customer extends BaseModel {
 	// 姓名
